e20a2fdb407fd46430d86cfc4910005eccc96a30
Stalyn Chiquito <StalynChiquito@mac-mini-de-stalyn.local>
2016-09-17 08:46:08 -0500

    Initial Commit

---
+++ juegomemoria/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  juegomemoria
+//
+//  Created by Stalyn Chiquito on 17/9/16.
+//  Copyright © 2016 Stalyn Chiquito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
